44f1e759bfa0cefc4c0aac7cb1607f749a248da4
Hunter Pfannenstiel <pfannenstielhunter@gmail.com>
2024-02-07 23:50:25 -0600

    Refactored NotePosition class

--- components/ui/measure/segment/index.tsx
+++ components/ui/measure/segment/index.tsx
@@ -17,21 +17,12 @@ const Segment: FunctionComponent<Props> = (props) => {
   const { belowBody, body = 9, aboveBody } = props;
   const { spaceToLineRatio = 0.25 } = props;
   const totalComponents = belowBody + body + aboveBody;
-  const lineCount = MeasureUtils.getLineCount(
-    totalComponents - 1,
-    !!props.startWithLine
-  );
-  const spaceCount = MeasureUtils.getSpaceCount(
-    totalComponents - 1,
-    !!props.startWithLine
-  );
-  const linePercentage = lineCount / totalComponents; //The percentage of the container that ALL of the lines get
-  const spacePercentage = 1 - linePercentage; //The percentage of the container that ALL of the spaces get
-
-  const percentPerLine =
-    (1 / lineCount) * linePercentage * (1 - spaceToLineRatio / 2); //Apply half the ratio to downsize the line and half to upsize the space
-  const percentPerSpace =
-    (1 / spaceCount) * spacePercentage * (1 + spaceToLineRatio / 2);
+  const { percentPerLine, percentPerSpace } =
+    MeasureUtils.getLedgerComponentHeights(
+      totalComponents,
+      spaceToLineRatio,
+      !!props.startWithLine
+    );
   const components = generateMeasureComponents(
     belowBody,
     aboveBody,
--- objects/note-position/index.ts
+++ objects/note-position/index.ts
@@ -2,25 +2,30 @@ type UnitMeasurement = "px" | "%";
 
 type Note = { x: number; y: number; length: number };
 
-export type Offset = { x: string; y: string };
+type ComponentHeights = { percentPerLine: number; percentPerSpace: number };
+
+export type Offset = { x: number; y: number };
 
 export default class NotePosition {
-  lineHeight: number;
-  spaceHeight: number;
+  heights: ComponentHeights;
   segmentLength: number;
-  unit: UnitMeasurement;
   startsWithLine: boolean;
+  measureHeight: number;
   constructor(
-    lineHeight: number,
-    spaceHeight: number,
+    componentCount: number,
     segmentLength: number,
-    unit: UnitMeasurement,
+
+    measureHeight: number,
+    spaceToLineRatio = 0.25,
     startWithLine = false
   ) {
-    this.lineHeight = lineHeight;
-    this.spaceHeight = spaceHeight;
+    this.heights = MeasureUtils.getLedgerComponentHeights(
+      componentCount,
+      spaceToLineRatio,
+      startWithLine
+    );
+    this.measureHeight = measureHeight;
     this.segmentLength = segmentLength; //Will be 25 if 4/4 time (1/4 * 100) w/ % units
-    this.unit = unit;
     this.startsWithLine = startWithLine;
   }
 
@@ -33,14 +38,14 @@ export default class NotePosition {
     let spaceCount = MeasureUtils.getSpaceCount(yPos, this.startsWithLine);
     let lineCount = MeasureUtils.getLineCount(yPos, this.startsWithLine);
     this.isOnLine(yPos) ? (lineCount -= 0.5) : (spaceCount -= 0.5);
-    const units = spaceCount * this.spaceHeight + lineCount * this.lineHeight;
-    return units + this.unit;
+    const spaceHeight = this.heights.percentPerSpace * this.measureHeight;
+    const lineHeight = this.heights.percentPerLine * this.measureHeight;
+    return spaceCount * spaceHeight + lineCount * lineHeight;
   }
 
   private getXOffset(xPos: number, length: number) {
     const centerOfLength = length / 2;
-    const center = (xPos + centerOfLength) * this.segmentLength;
-    return center + this.unit;
+    return (xPos + centerOfLength) * this.segmentLength;
   }
 
   public getNoteOffsets(notes: Note[]): Offset[] {
@@ -74,5 +79,29 @@ export class MeasureUtils {
     return startsWithLine
       ? this.getFirstComponentCount(yPos)
       : this.getSecondComponentCount(yPos);
+  }
+
+  static getLedgerComponentHeights(
+    componentCount: number,
+    spaceToLineRatio: number,
+    startWithLine: boolean
+  ) {
+    const lineCount = MeasureUtils.getLineCount(
+      componentCount - 1,
+      startWithLine
+    );
+    const spaceCount = MeasureUtils.getSpaceCount(
+      componentCount - 1,
+      startWithLine
+    );
+    const linePercentage = lineCount / componentCount; //The percentage of the container that ALL of the lines get
+    const spacePercentage = 1 - linePercentage; //The percentage of the container that ALL of the spaces get
+
+    const percentPerLine =
+      (1 / lineCount) * linePercentage * (1 - spaceToLineRatio / 2); //Apply half the ratio to downsize the line
+    const percentPerSpace =
+      (1 / spaceCount) * spacePercentage * (1 + spaceToLineRatio / 2); //Apply half the ratio to upsize the space
+
+    return { percentPerLine, percentPerSpace };
   }
 }
