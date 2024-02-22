f549a451831b8946433dfcba4ff0e1e25bb8405b
Hunter Pfannenstiel <pfannenstielhunter@gmail.com>
2024-02-21 21:24:48 -0600

    Rests now display. The quarter rest will render if rest type is not supported yet

--- components/ui/measure/display-measures/utils.tsx
+++ components/ui/measure/display-measures/utils.tsx
@@ -5,6 +5,7 @@ import { getNoteDuration } from "@/components/providers/music/utils";
 import { durationToNoteType } from "@/utils/music";
 import MeasureRest from "../measure-item/measure-rest";
 import { fractionToPercent } from "@/utils";
+import { ReactNode } from "react";
 
 export const getMeasureItems = (
   measures: Measure[],
@@ -16,17 +17,27 @@ export const getMeasureItems = (
 };
 
 const getItemsForMeasure = (measure: Measure, measurements: Measurement) => {
+  const components: ReactNode[] = [];
   let currX = 0;
-  return measure.notes.map((note) => {
-    return getDisplayNote(note, measurements);
+  measure.notes.forEach((note) => {
+    const noteDuration = getNoteDuration(note.type, 4);
+    const restLength = note.x - currX;
+    if (restLength) {
+      components.push(getDisplayRest(restLength, currX, 4, measurements));
+    }
+    components.push(getDisplayNote(note, measurements));
+    currX = note.x + noteDuration;
   });
+  const restLength = 4 - currX;
+  if (restLength) {
+    components.push(getDisplayRest(restLength, currX, 4, measurements));
+  }
+  return components;
 };
 
 const getDisplayNote = (note: Note, measurements: Measurement) => {
   const length = getNoteDuration(note.type, 4);
   const { x, y } = measurements.getNoteOffset({ ...note, length });
-  // console.log({ x, y });
-  // console.log(measurements.getSpaceFraction());
   const component = (
     <MeasureNote
       containerProps={{
@@ -50,7 +61,7 @@ const getDisplayRest = (
   const type = durationToNoteType(duration, beatNote);
   const { x, y } = measurements.getNoteOffset({
     x: xPos,
-    y: 5,
+    y: 8, //Replace this with the y value that is the middle of the measure
     length: duration,
   });
   const component = (
--- lib/notes/ui/rests/index.tsx
+++ lib/notes/ui/rests/index.tsx
@@ -1,11 +1,21 @@
 import { NoteType } from "@/components/providers/music/types";
 import { FunctionComponent } from "react";
 import EighthRest from "./eighth";
+import QuarterRest from "./quarter";
+import SixteenthRest from "./sixteenth";
 
-// const restComponentMap: {[key in NoteType]: FunctionComponent} = {
-//     "whole"
-// }
+const restComponentMap: { [key in NoteType]: FunctionComponent } = {
+  // whole: QuarterRest,
+  // "dotted-half": QuarterRest,
+  // half: QuarterRest,
+  // "dotted-quarter": QuarterRest,
+  quarter: QuarterRest,
+  // "dotted-eighth": QuarterRest,
+  eighth: EighthRest,
+  // "dotted-sixteenth": QuarterRest,
+  sixteenth: SixteenthRest,
+};
 
 export const getRestComponent = (type: NoteType) => {
-  return EighthRest;
+  return restComponentMap[type] || QuarterRest;
 };
